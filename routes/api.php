9099e34cd2179d157e78de99297619e3ae5e358d
umaregina <74174425+umaregina@users.noreply.github.com>
2021-07-27 03:49:04 +0700

    melakukan perubahan di file api.php

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,19 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/Customers', 'CustomersController@index');
+Route::get('/Customers/{customers}', 'CustomersController@show');
+Route::post('/Customers', 'CustomersController@store');
+Route::delete('/Customers/{customers}', 'CustomersController@destroy');
+Route::post('/Customers/{customers}', 'CustomersController@update');
+
+// Route::get('/Mahasiswa', 'MahasiswaController@index');
+// Route::get('/Mahasiswa/{mahasiswa}', 'MahasiswaController@show');
+// Route::post('/Mahasiswa', 'MahasiswaController@store');
+// Route::delete('/Mahasiswa/{mahasiswa}', 'MahasiswaController@destroy');
+// Route::post('/Mahasiswa/{mahasiswa}', 'MahasiswaController@update');
+// Route::get('/Dosen', 'DosenController@index');
+// Route::get('/Dosen/{dosen}', 'DosenController@show');
+// Route::post('/Dosen', 'DosenController@store');
+// Route::delete('/Dosen/{dosen}', 'DosenController@destroy');
+// Route::post('/Dosen/{dosen}', 'DosenController@update');
